5b6c1238b44016a7a690b8a1116443faa5c5cf30
morinikunj <morinikunj123@gmail.com>
2024-04-24 11:46:15 +0530

    some api correction
    some api correction

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -2,22 +2,33 @@ const Category = require("../models/category");
 
 const addCategory = async (req, res) => {
     try {
-        const { name, imageUrl } = req.body;
+        const { name, image } = req.body;
     
         // Create a new category document
         const newCategory = new Category({
           name,
-          imageUrl
+          image
         });
     
         // Save the category to the database
         await newCategory.save();
     
-        res.status(201).json(newCategory);
+        res.json({msg: name+" category added successfully."});
       } catch (err) {
         console.error(err);
         res.status(500).json({ error: 'Internal Server Error' });
       }
+}
+
+const getCategory = async (req, res) => {
+  try {
+   
+
+    res.json({msg: " category added successfully."});
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 }
 
 
--- backend/models/category.js
+++ backend/models/category.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema({
     name: { type: String, required: true },
-    imageUrl: { type: String, required: true }
+    image: { type: String, required: true }
   });
   
   // Define model
